b4717bf7b863d8ade9874c05bac2d119df986c89
Simon Le Pine <slepine@scheduled-dev.teksavvy.com>
2022-05-30 16:36:02 -0700

    -> Removing shared code from main git

--- src/classes/microservice.ts
+++ src/classes/microservice.ts
@@ -12,3 +12,48 @@ class test {
 
 test = new test;
 export default test;
+
+/**
+ * 
+import axios from 'axios';
+
+type User = {
+  id: number;
+  email: string;
+  first_name: string;
+};
+
+type GetUsersResponse = {
+  data: User[];
+};
+
+async function getUsers() {
+  try {
+    // const data: GetUsersResponse
+    const { data, status } = await axios.get<GetUsersResponse>(
+      'https://reqres.in/api/users',
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    );
+
+    console.log(JSON.stringify(data, null, 4));
+
+    // 👇️ "response status is: 200"
+    console.log('response status is: ', status);
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+getUsers();
