630f952a87ca5132f6fadfa78191f90666ce7b69
Simon Le Pine <sl@lakebed.io>
2022-10-27 14:20:34 -0700

    Starting from scratch

--- src/migrate_database-calendars.ts
+++
@@ -1,5 +0,0 @@
-module.exports = [
-	'CREATE TABLE IF NOT EXISTS "store" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT);', 
-	'ALTER TABLE "store" ADD "unique_hash" text(256) NULL;', 
-	'ALTER TABLE "store" ADD "json" NULL;'
-];
--- src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import send from '~routes/email/send'
-
-const index = {
-	'send' : send, 
-}
-export default index;
