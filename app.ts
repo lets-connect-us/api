88ab02baed454bd926d8f617340a04b7774e4baa
Simon Le Pine <sl@lakebed.io>
2022-10-28 10:28:47 -0700

    Testing render.com

--- app.ts
+++ app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 8001;
 
 app.listen(port, () => {
   console.log(`Timezones by location application is running on port ${port}.`);
+});
+
+
+
+app.get('/', (request, result) => {
+	result.send('Testing');
 });
